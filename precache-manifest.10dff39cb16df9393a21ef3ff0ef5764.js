27794df56bb76241a7af8ddccd38869e7e9f4ad1
Robert Bolender <bob@bitwisebob.com>
2019-07-12 21:05:30 -0700

    Updates

---
+++ precache-manifest.10dff39cb16df9393a21ef3ff0ef5764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d5988a1dbb6ba9a53fd57723ebcfaa",
+    "url": "/react-typescript-template/index.html"
+  },
+  {
+    "revision": "10f0a388c7d01c269806",
+    "url": "/react-typescript-template/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fa830bba37691e0693ba",
+    "url": "/react-typescript-template/static/js/2.1e9f73fb.chunk.js"
+  },
+  {
+    "revision": "10f0a388c7d01c269806",
+    "url": "/react-typescript-template/static/js/main.73a17082.chunk.js"
+  },
+  {
+    "revision": "da49a32df6c4a88cf891",
+    "url": "/react-typescript-template/static/js/runtime~main.97016d10.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-typescript-template/static/media/logo.5d5d9eef.svg"
+  }
+]);
