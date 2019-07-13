dcf67c0d2bc922d0c498ed5feb3e0c914e24bbf7
Robert Bolender <bob@bitwisebob.com>
2019-07-12 21:03:30 -0700

    feat: Update template to use Styled Components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
 import logo from "./logo.svg";
-import "./App.css";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Snow White and the Seven React Hooks</p>
-      </header>
-    </div>
+    <Wrapper>
+      <Header>
+        <Logo src={logo} className="App-logo" alt="logo" />
+        <p>React + TypeScript + Styled Components</p>
+      </Header>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const LogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Logo = styled.img`
+  animation: ${LogoSpin} infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+`;
 
 export default App;
